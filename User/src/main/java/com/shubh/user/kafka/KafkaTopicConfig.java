67aeba5493e077eedb7e1e4495216091b0a915fb
ShubhMehrotra <ShubhMehro@gmail.com>
2025-04-16 23:46:19 +0530

    Added Kafka config in user service

---
+++ User/src/main/java/com/shubh/user/kafka/KafkaTopicConfig.java
@@ -0,0 +1,34 @@
+package com.shubh.user.kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Value("$(app.kafka.topic)")
+    private String topicName;
+
+    @Bean
+    public NewTopic userCreatedTopic(){
+
+        return TopicBuilder.name(topicName)
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+
+
+    @Bean
+    public NewTopic deadLetterTopic()
+    {
+
+        return TopicBuilder.name(topicName+"_DLQ")
+                .partitions(3)
+                .replicas(3)
+                .build();
+    }
+}
---
+++ User/src/main/java/com/shubh/user/kafka/UserProducer.java
@@ -0,0 +1,20 @@
+package com.shubh.user.kafka;
+
+import com.shubh.user.dto.UserCreatedEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class UserProducer {
+    private final KafkaTemplate<String, UserCreatedEvent> kafkaTemplate;
+
+    @Value("${app.kafka.topic}")
+    private String topic;
+
+    public UserProducer(KafkaTemplate<String, UserCreatedEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
+        kafkaTemplate.send(topic, userCreatedEvent);
+    }
+}
